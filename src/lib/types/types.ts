fa2473ff519aafa1a2d64fd6fe445e3031a805f9
francis-villapando <francisvillapando222@gmail.com>
2026-09-20 23:31:07 +0800

    chore(types): prettier-format TermKind union

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -121,7 +121,14 @@ export interface SessionParticipation {
 export type Theory = "RBT" | "CLT" | "TTI";
 
 export type TermKind =
-  "issue" | "RBT" | "CLT" | "TTI" | "ILO" | "prevalence" | "topic" | "recommendation";
+  | "issue"
+  | "RBT"
+  | "CLT"
+  | "TTI"
+  | "ILO"
+  | "prevalence"
+  | "topic"
+  | "recommendation";
 
 export interface RecommendationTerm {
   text: string;
